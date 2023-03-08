30400a165631a49c125bbbe7a4b36bab94e1d2d1
sarveshXrajan <sarveshr03@gmail.com>
2023-03-07 18:47:09 -0700

    RED add test for count cities

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -46,7 +46,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int getCount(){
-        return cities.size();
+        return 0;
     }
 
     public void addCity(City city){
